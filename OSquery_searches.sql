6052d5e58a7e569deb5e48893274fe764b62b8e1
nscott <nls.inbox@gmail.com>
2018-07-08 10:14:04 -0700

    First Commit
    basic sql querys for osquery

--- OSquery_searches.sql
+++ OSquery_searches.sql
@@ -1,21 +1,8 @@
-# osquery examples
 
-
-
-![alt text](https://cdn-images-1.medium.com/max/1600/1*XC-k2QigREIwZnBpFZ4StA@2x.png "osquery")
-
-Query information about your devices with basic sql syntax.
-
-<https://osquery.io>
-
-#### Examples:
-
-```sql
-
---show users accounts
+--show users
 SELECT * FROM users WHERE uid > 500;
 
---show all firewall exceptions
+--show firewall exceptions
 SELECT * FROM alf_exceptions;
 
 --applications that have never been opened
@@ -24,8 +11,11 @@ SELECT name, bundle_version,path,last_opened_time  FROM apps WHERE path LIKE "/A
 --show third party kernal exstentions
 SELECT * FROM kernel_extensions WHERE name NOT LIKE "com.apple.%";
 
--- show applications and versions
+-- show apps
 SELECT name, bundle_version,path,last_opened_time  FROM apps WHERE path LIKE "/Applications%";
+
+--applications that have never been opened
+SELECT name, bundle_version,path,last_opened_time  FROM apps WHERE path LIKE "/Applications%" AND last_opened_time < 1;
 
 --show logged in console users
 SELECT * FROM logged_in_users WHERE tty LIKE "console";
@@ -41,4 +31,3 @@ SELECT * FROM interface_addresses WHERE interface LIKE "en%" AND address LIKE "%
 
 --join to see failed login attempts
 osquery> SELECT creation_time,failed_login_count,failed_login_timestamp,username,users.uid FROM account_policy_data INNER JOIN users on account_policy_data.uid = users.uid;
-```
