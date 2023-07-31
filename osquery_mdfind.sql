a362cf1311c5d11fccf55e7637c2aca78354119e
nic scott <nls.inbox@gmail.com>
2023-07-31 17:57:05 -0400

    Add: apps not in main apps folder

--- osquery_mdfind.sql
+++ osquery_mdfind.sql
@@ -27,3 +27,14 @@ SELECT f.path FROM file AS f JOIN mdfind ON mdfind.path = f.path
     AND mdfind.path NOT LIKE "%Library_Application_Support%"  
     LIMIT 10;
 
+
+-- find .apps not in the main /Appllicstions folder
+SELECT f.path FROM file AS f JOIN mdfind ON mdfind.path = f.path 
+    AND mdfind.query = "kMDItemKind == 'Application'" 
+    WHERE mdfind.path Not LIKE "%Applications%" 
+    AND mdfind.path Not LIKE "%System_Library%"
+    AND mdfind.path Not LIKE "%Users_%_Library_CloudStorage%"
+    AND mdfind.path Not LIKE "%Library_Application_Support%";
+
+   
+
